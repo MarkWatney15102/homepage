17e7be1fd727a6e09f59feb0061a34a8c817f856
Eike Eric Wientjes <eike.wientjes@eike-wientjes.de>
2022-10-20 00:07:11 +0200

    ADD: server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-// server.js
-const https = require('https')
-const { parse } = require('url')
-const next = require('next')
+const https = require('https');
 const fs = require('fs');
+const path = require('path')
+const { parse } = require('url');
 
+const next = require('next')
 const dev = false
 const hostname = 'eike-wientjes.de'
 const port = 443
@@ -17,27 +17,11 @@ var options = {
 };
 
 app.prepare().then(() => {
-    https.createServer(options,async (req, res) => {
-        try {
-            // Be sure to pass `true` as the second argument to `url.parse`.
-            // This tells it to parse the query portion of the URL.
-            const parsedUrl = parse(req.url, true)
-            const { pathname, query } = parsedUrl
-
-            if (pathname === '/a') {
-                await app.render(req, res, '/a', query)
-            } else if (pathname === '/b') {
-                await app.render(req, res, '/b', query)
-            } else {
-                await handle(req, res, parsedUrl)
-            }
-        } catch (err) {
-            console.error('Error occurred handling', req.url, err)
-            res.statusCode = 500
-            res.end('internal server error')
-        }
-    }).listen(port, (err) => {
+    https.createServer(options, async (req, res) => {
+        const parsedUrl = parse(req.url, true);
+        await handle(req, res, parsedUrl);
+    }).listen(port, err => {
         if (err) throw err
-        console.log(`> Ready on http://${hostname}:${port}`)
+        console.log(`> Ready on localhost:${port}`)
     })
 })
