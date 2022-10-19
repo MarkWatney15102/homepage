cade3b369c2be64a5c046ef3de183d02d1bf29c4
Eike Eric Wientjes <eike.wientjes@eike-wientjes.de>
2022-10-19 22:52:28 +0200

    ADD: Api route to check if is running for test on live system

---
+++ pages/api/status.js
@@ -0,0 +1,25 @@
+export default function handler(req, res) {
+    const body = req.body;
+
+    let message = "";
+    let status = 400;
+    let running = false;
+    if (body.token) {
+        if (body.token === 'jfkg5d8b79h3d4') {
+            message = "Running";
+            status = 200;
+            running = true;
+        } else {
+            message = "Token is wrong";
+        }
+    } else {
+        message = "Token is wrong";
+    }
+
+    res.status(status).json(
+        {
+            message,
+            running
+        }
+    )
+}
