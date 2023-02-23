8b5c6a4ae652984e385d3a10ba5becba22fbfb4e
Eike Eric Wientjes <eike.wientjes@eike-wientjes.de>
2023-02-23 18:56:13 +0100

    DEV

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const https = require('https');
+const http = require('http');
 const fs = require('fs');
 const path = require('path')
 const { parse } = require('url');
 
-const next = require('next')
-const dev = false
-const hostname = 'eike-wientjes.de'
-const port = 443
-// when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port })
-const handle = app.getRequestHandler()
+const next = require('next');
+const dev = false;
+const hostname = '89.58.29.202';
+const port = 80;
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
 
 var options = {
     key: fs.readFileSync('ssl/key.pem'),
@@ -17,11 +17,18 @@ var options = {
 };
 
 app.prepare().then(() => {
-    https.createServer(options, async (req, res) => {
+    http.createServer(async (req, res) => {
         const parsedUrl = parse(req.url, true);
         await handle(req, res, parsedUrl);
     }).listen(port, err => {
         if (err) throw err
         console.log(`> Ready on localhost:${port}`)
-    })
+    });
+
+    //     const parsedUrl = parse(req.url, true);
+    //     await handle(req, res, parsedUrl);
+    // }).listen(port, err => {
+    //     if (err) throw err
+    //     console.log(`> Ready on localhost:${port}`)
+    // })
 })
