15cd1731d52830187d87cf8b646db8cf08e67e7e
Eike Eric Wientjes <eike.wientjes@eike-wientjes.de>
2023-03-27 13:51:52 +0200

    ADD: server https

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
 const https = require('https');
 const http = require('http');
 const fs = require('fs');
-const path = require('path')
 const { parse } = require('url');
 
 const next = require('next');
 const dev = false;
 const hostname = '89.58.29.202';
-const port = 80;
+const port = 443;
+const portHttp = 80;
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
-// var options = {
-//     key: fs.readFileSync('ssl/key.pem'),
-//     cert: fs.readFileSync('ssl/cert.pem'),
-// };
+const certDir = `/etc/letsencrypt/live`;
+const domain = `eike-eric-wientjes.de`;
+const options = {
+    key: fs.readFileSync(`${certDir}/${domain}/privkey.pem`),
+    cert: fs.readFileSync(`${certDir}/${domain}/fullchain.pem`)
+};
 
 app.prepare().then(() => {
     http.createServer(async (req, res) => {
+        res.redirect('https://' + req.headers.host + req.url);
+    }).listen(portHttp, err => {
+        if (err) throw err
+        console.log(`> Ready on localhost:${port}`)
+    });
+
+    https.createServer(options, async (req, res) => {
         const parsedUrl = parse(req.url, true);
         await handle(req, res, parsedUrl);
     }).listen(port, err => {
         if (err) throw err
         console.log(`> Ready on localhost:${port}`)
     });
-
-    //     const parsedUrl = parse(req.url, true);
-    //     await handle(req, res, parsedUrl);
-    // }).listen(port, err => {
-    //     if (err) throw err
-    //     console.log(`> Ready on localhost:${port}`)
-    // })
 })
